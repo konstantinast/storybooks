260c91f5de314220e8f9c32f1414ba7449dbaccf
Konstantinas <konstantinast@gmail.com>
2018-02-07 17:04:40 +0200

    handled google oauth process

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://main-user:tanka5@ds125938.mlab.com:25938/storybooks-dev',
+    googleClientID: '401330767348-o3dm2ppnd4ejfme3uon4teg00npj1q4j.apps.googleusercontent.com',
+    googleClientSecret: '-UWxXCwEoVQZXTj4ofzUzGBS'
+};
